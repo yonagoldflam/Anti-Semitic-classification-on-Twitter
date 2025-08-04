3d2d8964bba9a935fc40f08dd6c1ad9b7f0d9391
tzirkus <zalmen5779@gmail.com>
2025-08-04 18:15:52 +0300

    finish the files name

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ from manager import Manager
 class Main:
     def __init__(self):
         self.data_url = "../data/tweets_dataset.csv"
+        cleaned_data_url = "../data/tweets_dataset_cleaned.csv"
+        anlyzed_data_url = "../data/result.json"
         self.manager = Manager(self.data_url)
         self.manager.analyse_data()
         self.manager.da.print_result_dict()
         self.manager.cleaning_data()
+        self.manager.write_to_json_file(anlyzed_data_url)
+        self.manager.write_to_csv_file(cleaned_data_url)
 
 
 
--- src/write_data_files.py
+++ src/write_data_files.py
@@ -1,10 +1,9 @@
 import json
-import pandas as pd
 
 class WriteDataFiles:
-    def write_to_json_file(dict, filename):
-        with open(f'../data/{filename}', 'w') as f:
+    def write_to_json_file(dict, anlyzed_data_url):
+        with open(anlyzed_data_url, 'w') as f:
             json.dump(dict, f)
 
-    def write_to_csv_file(df, filename):
-        df.to_csv(f'../data/{filename}', index=False)
+    def write_to_csv_file(df, cleaned_data_url):
+        df.to_csv(cleaned_data_url, index=False)
