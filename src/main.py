015414543d6576b16b66d702a0d859926553c4a0
tzirkus <zalmen5779@gmail.com>
2025-08-04 10:18:38 +0300

    fix url

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from manager import Manager
 class Main:
     def __init__(self):
 
-        self.data_url = "anti-semitic_on_twitter/data/tweets_dataset.csv"
+        self.data_url = "../data/tweets_dataset.csv"
         self.manager = Manager(self.data_url)
 
 if __name__ == "__main__":
